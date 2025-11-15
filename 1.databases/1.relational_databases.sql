7f85ab1df8118e9a2a01c557ed78cacf9e31d190
Gulshir <gulshirislam@gmail.com>
2025-11-15 21:30:27 +0530

    project setup

---
+++ 1.databases/1.relational_databases.sql
@@ -0,0 +1,6 @@
+-- create a schema for social network. Example: users, profiles, posts, photos, following etc
+-- define relationship between them.
+
+-- try inserting data in users and profiles tables in a single transaction. Scenario is when user registers into 
+-- the application, row is created in both tables. 
+-- Hint: users table mostly used for auth, profiles table for complete user information.
