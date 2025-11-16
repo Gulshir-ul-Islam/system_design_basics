a0f466a6bce5d42f6936147b0080a84b1847307b
Gulshir <gulshirislam@gmail.com>
2025-11-16 13:52:48 +0530

    final changes to social media sample table schemas and queries

--- 1.databases/1.relational_databases_social_media.sql
+++ 1.databases/1.relational_databases_social_media.sql
@@ -52,11 +52,45 @@ CREATE TABLE posts(
     post_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     photo_id INT, -- if the post is content based
-    content TEXT NOT NULL,
+    content TEXT, -- this can be null if the post is only content based
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (photo_id) REFERENCES photos(photo_id)
 );
 
--- pending - transaction sample
+-- user registered into the application
+START TRANSACTION;
+
+INSERT INTO users(username, email, password_hash) VALUES('Andrew Symonds', 'andrew.sy@gmail.com', 'iusk97t6gthguw');
+
+SET @last_user_id = LAST_INSERT_ID();
+
+SET @file_path = CONCAT(CURRENT_TIMESTAMP, '.jpeg');
+INSERT INTO photos(user_id, image_path) VALUES(@last_user_id, CONCAT(@last_user_id,'/content/images/', ));
+
+SET @last_photo_id = LAST_INSERT_ID();
+
+INSERT INTO profiles(user_id, profile_photo_id, bio, date_of_birth, address, phone) 
+    VALUES(@last_user_id, @last_photo_id, 'cricketer', '1985-10-12', 'Sydney', '+21-890756456');
+
+COMMIT;
+-- end of transaction
+
+SELECT * FROM users;
+SELECT * FROM profiles;
+SELECT * FROM photos;
+
+-- Q: how to get all photos (photo IDs) except profile photo given the user ID? 
+-- Should have I added additional column called is_profile_photo in 'photos' table and mark 'true' for profile photo
+-- and for rest set default as 'false' - this would be unnecessary and simply waste of storage. This could be easily handled 
+-- by the application only using the join right query
+SELECT 
+    p.photo_id 
+FROM 
+    photos p
+INNER JOIN 
+    profiles pr ON p.user_id = pr.user_id
+WHERE 
+    p.user_id = 1 
+    AND p.photo_id != pr.profile_photo_id;
